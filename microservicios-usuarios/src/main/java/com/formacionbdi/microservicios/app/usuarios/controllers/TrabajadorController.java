61f2e7bae45b80a80787ebec106b5d2500fc7d46
Sussan Hernandez <sussanaracely@gmail.com>
2023-03-01 12:32:14 -0300

    CrossOrigin

--- microservicios-usuarios/src/main/java/com/formacionbdi/microservicios/app/usuarios/controllers/TrabajadorController.java
+++ microservicios-usuarios/src/main/java/com/formacionbdi/microservicios/app/usuarios/controllers/TrabajadorController.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,6 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.formacionbdi.microservicios.app.usuarios.models.entity.Trabajador;
 import com.formacionbdi.microservicios.app.usuarios.services.TrabajadorService;
 
+@CrossOrigin(origins = "http://localhost:4200/")
 @RestController
 @RequestMapping("api/trabajadores")
 public class TrabajadorController {
